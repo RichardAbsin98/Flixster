7faf603b956e7d5d4440fe89690cba80da6d84d2
Richard Absin <richard.absin24@outlook.com>
2019-01-27 11:08:56 -0800

    Added basic UICollectionView functionality and worked with the layout of the MovieDetails

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Richard Absin on 1/27/19.
+//  Copyright © 2019 richard.absin24@outlook.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
